7f8e282b703733f735eab21d9203a86fb2ad7269
Jake Lim <jiahao89sg@gmail.com>
2025-12-29 18:57:04 +0800

    upgrade to https

--- generate_certs.sh
+++ generate_certs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mkdir -p certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx.key -out certs/nginx.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
-echo "Certificates generated in certs/"
+
+certs_dir="./src-nginx/certs"
+
+mkdir -p $certs_dir
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $certs_dir/nginx.key -out $certs_dir/nginx.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
+echo "Certificates generated in $certs_dir/"
